dc4d57ddf8fce06146cf7181bebb236b501c9701
Mati <Mati@Matias>
2015-03-31 16:00:02 -0300

    agrego lo que faltaba

---
+++ xtend-gen/ar/edu/pablitar/apiario/Golondrina.java
@@ -0,0 +1,16 @@
+package ar.edu.pablitar.apiario;
+
+@SuppressWarnings("all")
+public class Golondrina {
+  /* @Accessors
+   */private int energia = 100;
+  
+  /**
+   * Este metodo hace volar a la golondrina tantos km como se indique, agotando parte de su energia
+   */
+  public Object vola(final int km) {
+    throw new Error("Unresolved compilation problems:"
+      + "\n-= cannot be resolved."
+      + "\n* cannot be resolved.");
+  }
+}
---
+++ xtend-gen/ar/edu/pablitar/apiario/GolondrinaTestSuite.java
@@ -0,0 +1,20 @@
+package ar.edu.pablitar.apiario;
+
+@SuppressWarnings("all")
+public class GolondrinaTestSuite {
+  /* @Test
+   */public void unaGolondrinaVuelaYPierdeEnergia() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field Assert is undefined for the type GolondrinaTestSuite"
+      + "\nThe field energia is not visible"
+      + "\nassertEquals cannot be resolved");
+  }
+  
+  /* @Test
+   */public void unaGolondrinaVuelaMasYPierdeMasEnergia() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field Assert is undefined for the type GolondrinaTestSuite"
+      + "\nThe field energia is not visible"
+      + "\nassertEquals cannot be resolved");
+  }
+}
